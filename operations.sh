deb6891260cbd51e4954595dd64544764dd598e1
Tahjae <tahjaedev@proton.me>
2024-06-12 11:19:14 -0400

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -1,3 +1,1 @@
-wget --no-check-certificate --content-disposition https://github.com/Tahj-S/Operations-Calculator/releases/tag/v1.0.0-alpha
-# --no-check-cerftificate was necessary for me to have wget not puke about https
 curl -LJO https://github.com/Tahj-S/Operations-Calculator/releases/tag/v1.0.0-alpha
